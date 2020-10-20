9bda88d5199fe76e0bcbd18f023e394a93ea93cd
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2020-10-20 23:54:31 +0100

    Repository to user

---
+++ src/main/java/com/online/loja/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.online.loja.repository;
+
+import com.online.loja.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findBySurname(String surname);
+
+}
