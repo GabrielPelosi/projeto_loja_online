390a6af1be6e2f94918284cb3ff36bb312a365c7
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2020-11-04 23:24:30 +0000

    Login payload

---
+++ src/main/java/com/online/loja/payload/LoginRequest.java
@@ -0,0 +1,25 @@
+package com.online.loja.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    // Getters and Setters (Omitted for brevity)
+}
