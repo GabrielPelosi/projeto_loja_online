f84b45adc7521e882a1e9b85b09ae1aae527b2e2
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2020-11-04 23:23:08 +0000

    enable configuration

--- src/main/java/com/online/loja/LojaApplication.java
+++ src/main/java/com/online/loja/LojaApplication.java
@@ -1,13 +1,22 @@
 package com.online.loja;
 
+import com.online.loja.config.oauth.AppOAuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppOAuthProperties.class)
 public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
