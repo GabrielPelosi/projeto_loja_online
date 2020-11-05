19389d5f2f75e98e76b404645bc51fc55fab1e90
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2020-11-05 00:11:20 +0000

    commit

---
+++ src/main/java/com/online/loja/payload/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.online.loja.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+}
