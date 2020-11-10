8862a060c1709298a5d1865ce96a52cabcc271c9
GabrielPelosi <ggpelosi21@gmail.com>
2020-11-10 13:19:27 +0000

    user rep

--- src/main/java/com/online/loja/security/repository/UserRepository.java
+++ src/main/java/com/online/loja/security/repository/UserRepository.java
@@ -1,14 +1,17 @@
-package com.online.loja.repository;
+package com.online.loja.security.repository;
+
 
 import com.online.loja.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findBySurname(String surname);
+    Boolean existsByEmail(String email);
 
 }
