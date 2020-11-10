a9e1326a34e66bd44e76f5309dbf06bb6ad74f17
GabrielPelosi <ggpelosi21@gmail.com>
2020-11-10 13:21:10 +0000

    security constants

---
+++ src/main/java/com/online/loja/security/config/SecurityConstants.java
@@ -0,0 +1,16 @@
+package com.online.loja.security.config;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "SECRET_KEY";
+    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users/register";
+
+    public static final String PRODUCTS_AVAILABLE_URL = "/products";
+    public static final String LOGIN_URL = "/users/login";
+    public static final String HAS_ADMIN_ROLE = "hasRole('ROLE_ADMIN')";
+    public static final String HAS_USER_ROLE = "hasRole('ROLE_USER')";
+
+}
