8912a7aa6ce1401c7863aeba267d28425647f327
GabrielPelosi <ggpelosi21@gmail.com>
2020-11-10 13:20:09 +0000

    payload

---
+++ src/main/java/com/online/loja/payload/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.online.loja.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Slf4j
+public class RegisterRequest {
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+}
