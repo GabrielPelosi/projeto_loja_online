d396a4fba9886986913d32d22a20a3394615e262
GabrielPelosi <ggpelosi21@gmail.com>
2020-11-11 16:46:25 +0000

    payload response

--- src/main/java/com/online/loja/payload/LoginResponse.java
+++ src/main/java/com/online/loja/payload/LoginResponse.java
@@ -5,10 +5,13 @@ import lombok.Data;
 
 @Data
 public class LoginResponse {
+    private final String email;
+    private final String name;
     private final String jwttoken;
 
-    public LoginResponse(String jwttoken) {
+    public LoginResponse(String email, String name, String jwttoken) {
+        this.email = email;
+        this.name = name;
         this.jwttoken = jwttoken;
     }
-
 }
