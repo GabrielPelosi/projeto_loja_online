c12c40760f074a0b11b6a7bb761c093be4dbb106
GabrielPelosi <ggpelosi21@gmail.com>
2020-11-20 16:45:34 +0000

    purchase now has email user

--- src/main/java/com/online/loja/repository/PurchaseRepository.java
+++ src/main/java/com/online/loja/repository/PurchaseRepository.java
@@ -5,6 +5,11 @@ import com.online.loja.repository.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase,Long> {
+
+
+    List<Purchase> findByEmailUser(String emailUser);
 }
