77ac1385ad7665a4814521dfbd04131372932ccb
GabrielPelosi <gabriel.pelosi.2020@gmail.com>
2021-02-09 16:45:31 +0000

    ISSUE #18 - Creating tests to Category Repository

--- src/main/java/com/online/loja/repository/entity/Category.java
+++ src/main/java/com/online/loja/repository/entity/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -21,6 +23,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @NotNull
     private String name;
 
 
---
+++ src/test/java/com/online/loja/repository/CategoryRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.online.loja.repository;
+
+import com.online.loja.repository.entity.Category;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Test to Category Repositry")
+class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("Test to sve category when successful scenario")
+    public void save_category_successful(){
+        Category categoryToBeSaved = Category.builder().name("Empada").build();
+
+        Category savedCategory = categoryRepository.save(categoryToBeSaved);
+
+        Assertions.assertThat(savedCategory.getName()).isNotNull();
+        Assertions.assertThat(savedCategory.getName()).isNotBlank();
+        Assertions.assertThat(savedCategory.getName()).isEqualTo(categoryToBeSaved.getName());
+    }
+
+    @Test
+    @DisplayName("Test to sve category with blank name")
+    public void save_category__not_successful_blank_name(){
+        Category categoryToBeSaved = Category.builder().name(" ").build();
+
+        Assertions.assertThatThrownBy(() -> {
+            Category savedCategory = categoryRepository.save(categoryToBeSaved);
+        }).isInstanceOf(ConstraintViolationException.class)
+                .hasMessageContaining("must not be blank");
+
+    }
+
+    @Test
+    @DisplayName("Test to sve category with null name")
+    public void save_category__not_successful_null_name(){
+        Category categoryToBeSaved = Category.builder().name(null).build();
+
+        Assertions.assertThatThrownBy(() -> {
+            Category savedCategory = categoryRepository.save(categoryToBeSaved);
+        }).isInstanceOf(ConstraintViolationException.class)
+                .hasMessageContaining("must not be null");
+
+    }
+}
